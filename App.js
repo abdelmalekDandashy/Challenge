2fbf47e878e9341af05911dc997f029e7332e903
abdelmalekDandashy <dandasheh5@gmail.com>
2020-11-27 04:43:32 +0200

    feat: initialized redux store and reducer

--- App.js
+++ App.js
@@ -5,110 +5,48 @@
  * @format
  * @flow strict-local
  */
-
+/* eslint-disable */
 import React from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
   View,
   Text,
-  StatusBar,
+  StyleSheet
 } from 'react-native';
+import { createStore, combineReducers } from 'redux'; 
+import TopArticlesReducer from './store/Reducers/TopArticles'; 
+import { Provider } from 'react-redux'; 
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
+const App = () => {
+
+  const rootReducer = combineReducers({ 
+    meals: TopArticlesReducer 
+  });
+  
+  
+  const store = createStore(rootReducer); 
+  
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+
+    <Provider store={store}> 
+        <View style={styles.Body}>
+           <Text >Hellossss</Text>
+     
+        </View>
+    </Provider>
+
+    
+
+    
   );
+
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  Body: {
+    backgroundColor: 'red',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+ 
 });
 
 export default App;
---
+++ store/Reducers/TopArticles.js
@@ -0,0 +1,14 @@
+
+/* eslint-disable */
+
+
+const initialState = { 
+    
+    TopArticles: [] 
+};
+
+ const TopArticlesReducer = (state = initialState, action) => {
+    return state;
+ }
+
+export default TopArticlesReducer;
